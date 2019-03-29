15dea7cc17c1069fba821bc9b3c109cd10cbe1c5
Bartosz Nowak <mail@bnowak.com.pl>
2019-03-29 16:17:40 +0100

    move filters out of try/except block

--- src/postprocess.py
+++ src/postprocess.py
@@ -17,117 +17,117 @@ def postprocess_messages(name):
         print('Error: Failed to open %s.' % config.get('FilePaths', 'generated_messages'))
         sys.exit(1)
 
+    proper_messages = []
+    for item in messages:
+        if re.match(r'^xd', item):
+            item = item.upper()
+        if 'xd' in item:
+            item = re.sub(r'(xd{1,})', 'XD', item)
+        if re.match(r'(^\.\s)', item):
+            item = re.sub(r'(^\.\s)', '.', item)
+        if re.match(r'(^\,\s)', item):
+            item = re.sub(r'(^\,\s)', ',', item)
+        if '$ ' in item:
+            item = re.sub(r'(^\$\s)+', '$', item)
+        if '! ' in item:
+            item = re.sub(r'(\!\s)+', '!', item)
+        if ': ' in item:
+            item = re.sub(r'(\:\s)+', ':', item)
+        if '; ' in item:
+            item = re.sub(r'(\;\s)+', ';', item)
+        if '- ' in item:
+            item = re.sub(r'(\-\s)+', '-', item)
+        if '+ ' in item:
+            item = re.sub(r'(\+\s)+', '+', item)
+        if '< ' in item:
+            item = re.sub(r'(\<\s)+', '<', item)
+        if '> ' in item:
+            item = re.sub(r'(\>\s)+', '>', item)
+        if '^ ' in item:
+            item = re.sub(r'(\^\s)+', '^', item)
+        if '[ ' in item:
+            item = re.sub(r'(\[\s)+', '[', item)
+        if '* ' in item:
+            item = re.sub(r'(\*\s)+', '*', item)
+        if '@ ' in item:
+            item = re.sub(r'(\@\s)+', '@', item)
+        if '# ' in item:
+            item = re.sub(r'(\#\s)+', '#', item)
+        if '_ ' in item:
+            item = re.sub(r'(\_\s)+', '_', item)
+        if '( ' in item:
+            item = re.sub(r'(\(\s)+', '(', item)
+        if '~ ' in item:
+            item = re.sub(r'(\~\s)+', '~', item)
+        if '= ' in item:
+            item = re.sub(r'(\=\s)+', '=', item)
+        if ' \' ' in item:
+            item = re.sub(r'(\s\'\s)+', '\'', item)
+        if ' \" ' in item:
+            item = re.sub(r'(\s\"\s)+', '\"', item)
+        if '\' ' in item:
+            item = re.sub(r'(\'\s)+', '\'', item) 
+        if '\" ' in item:
+            item = re.sub(r'(\"\s)+', '\"', item) 
+        if '/ ' in item:
+            item = re.sub(r'(\/\s)+', '/', item)
+        if ' /' in item:
+            item = re.sub(r'(\s\/)+', '/', item) 
+        if ' ?' in item:
+            item = re.sub(r'(\s\?)+', '?', item)
+        if ' ,' in item:
+            item = re.sub(r'(\s\,)+', ',', item)
+        if ' .' in item:
+            item = re.sub(r'(\s\.)+', '.', item)
+        if ' )' in item:
+            item = re.sub(r'(\s\))+', ')', item)
+        if ' \'' in item:
+            item = re.sub(r'(\s\')+', '\'', item) 
+        if ' -' in item:
+            item = re.sub(r'(\s\-)+', '-', item)
+        if ' :' in item:
+            item = re.sub(r'(\s\:)+', ':', item)
+        if ' ;' in item:
+            item = re.sub(r'(\s\;)+', ';', item)
+        if ' ]' in item:
+            item = re.sub(r'(\s\])+', ']', item)
+        if ' $' in item:
+            item = re.sub(r'(\s\$)+', '$', item)
+        if ' %' in item:
+            item = re.sub(r'(\s\%)+', '%', item)
+        if ' =' in item:
+            item = re.sub(r'(\s\=)+', '=', item)
+        if ' _' in item:
+            item = re.sub(r'(\s\_)+', '', item)
+        if item is '':
+            continue
+        if len(item) > 100 or len(item) < 3:
+            continue
+        if item.startswith(('$', '.', ',', '[', ']', 'pulltop', ' ', 'k !', 't !', '^', '`',
+                            '!', '%', '\"', '\\', '/', '\'', '-', '~', '*', ':+', '+', '|',
+                            ')', ';\"', '? ', '#', '@ ', '(^', '{', '}')):
+            continue
+        if '@someone' in item:
+            continue
+        if re.match(r'^;[A-z]*', item):
+            continue
+        if re.match(r'^:[A-z]*', item):
+            continue
+        if re.match(r'^([A-z]\.).*', item):
+            continue
+        if re.match(r'^([A-z]\,).*', item):
+            continue
+        if re.match(r'^([A-z]\").*', item):
+            continue
+        if re.match(r'.*\".*\".*\".*', item):
+            continue
+        if re.match(r'.*\,.*\,.*\,.*', item):
+            continue
+        if re.match(r'.*\..*\..*\..*\..*', item):
+            continue
+        proper_messages.append(item)
+    
     try:
-        proper_messages = []
-        for item in messages:
-            if re.match(r'^xd', item):
-                item = item.upper()
-            if 'xd' in item:
-                item = re.sub(r'(xd{1,})', 'XD', item)
-            if re.match(r'(^\.\s)', item):
-                item = re.sub(r'(^\.\s)', '.', item)
-            if re.match(r'(^\,\s)', item):
-                item = re.sub(r'(^\,\s)', ',', item)
-            if '$ ' in item:
-                item = re.sub(r'(^\$\s)+', '$', item)
-            if '! ' in item:
-                item = re.sub(r'(\!\s)+', '!', item)
-            if ': ' in item:
-                item = re.sub(r'(\:\s)+', ':', item)
-            if '; ' in item:
-                item = re.sub(r'(\;\s)+', ';', item)
-            if '- ' in item:
-                item = re.sub(r'(\-\s)+', '-', item)
-            if '+ ' in item:
-                item = re.sub(r'(\+\s)+', '+', item)
-            if '< ' in item:
-                item = re.sub(r'(\<\s)+', '<', item)
-            if '> ' in item:
-                item = re.sub(r'(\>\s)+', '>', item)
-            if '^ ' in item:
-                item = re.sub(r'(\^\s)+', '^', item)
-            if '[ ' in item:
-                item = re.sub(r'(\[\s)+', '[', item)
-            if '* ' in item:
-                item = re.sub(r'(\*\s)+', '*', item)
-            if '@ ' in item:
-                item = re.sub(r'(\@\s)+', '@', item)
-            if '# ' in item:
-                item = re.sub(r'(\#\s)+', '#', item)
-            if '_ ' in item:
-                item = re.sub(r'(\_\s)+', '_', item)
-            if '( ' in item:
-                item = re.sub(r'(\(\s)+', '(', item)
-            if '~ ' in item:
-                item = re.sub(r'(\~\s)+', '~', item)
-            if '= ' in item:
-                item = re.sub(r'(\=\s)+', '=', item)
-            if ' \' ' in item:
-                item = re.sub(r'(\s\'\s)+', '\'', item)
-            if ' \" ' in item:
-                item = re.sub(r'(\s\"\s)+', '\"', item)
-            if '\' ' in item:
-                item = re.sub(r'(\'\s)+', '\'', item) 
-            if '\" ' in item:
-                item = re.sub(r'(\"\s)+', '\"', item) 
-            if '/ ' in item:
-                item = re.sub(r'(\/\s)+', '/', item)
-            if ' /' in item:
-                item = re.sub(r'(\s\/)+', '/', item) 
-            if ' ?' in item:
-                item = re.sub(r'(\s\?)+', '?', item)
-            if ' ,' in item:
-                item = re.sub(r'(\s\,)+', ',', item)
-            if ' .' in item:
-                item = re.sub(r'(\s\.)+', '.', item)
-            if ' )' in item:
-                item = re.sub(r'(\s\))+', ')', item)
-            if ' \'' in item:
-                item = re.sub(r'(\s\')+', '\'', item) 
-            if ' -' in item:
-                item = re.sub(r'(\s\-)+', '-', item)
-            if ' :' in item:
-                item = re.sub(r'(\s\:)+', ':', item)
-            if ' ;' in item:
-                item = re.sub(r'(\s\;)+', ';', item)
-            if ' ]' in item:
-                item = re.sub(r'(\s\])+', ']', item)
-            if ' $' in item:
-                item = re.sub(r'(\s\$)+', '$', item)
-            if ' %' in item:
-                item = re.sub(r'(\s\%)+', '%', item)
-            if ' =' in item:
-                item = re.sub(r'(\s\=)+', '=', item)
-            if ' _' in item:
-                item = re.sub(r'(\s\_)+', '', item)
-            if item is '':
-                continue
-            if len(item) > 100 or len(item) < 3:
-                continue
-            if item.startswith(('$', '.', ',', '[', ']', 'pulltop', ' ', 'k !', 't !', '^', '`',
-                                '!', '%', '\"', '\\', '/', '\'', '-', '~', '*', ':+', '+', '|',
-                                ')', ';\"', '? ', '#', '@ ', '(^', '{', '}')):
-                continue
-            if '@someone' in item:
-                continue
-            if re.match(r'^;[A-z]*', item):
-                continue
-            if re.match(r'^:[A-z]*', item):
-                continue
-            if re.match(r'^([A-z]\.).*', item):
-                continue
-            if re.match(r'^([A-z]\,).*', item):
-                continue
-            if re.match(r'^([A-z]\").*', item):
-                continue
-            if re.match(r'.*\".*\".*\".*', item):
-                continue
-            if re.match(r'.*\,.*\,.*\,.*', item):
-                continue
-            if re.match(r'.*\..*\..*\..*\..*', item):
-                continue
-            proper_messages.append(item)
-
         with open(config.get('FilePaths', 'messages'), 'w', encoding='utf8') as f:
             f.write(''.join(proper_messages))
     except:
